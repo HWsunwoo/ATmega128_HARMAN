21b0c5a6585ca05def11f00b6aa426b3fab6f0a8
HWsunwoo <tommy7791@naver.com>
2024-10-02 16:22:53 +0900

    2

---
+++ Test01/Test01/main.c
@@ -0,0 +1,20 @@
+/*
+ * Test01.c
+ *
+ * Created: 2024-10-02 오후 4:10:16
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+		printf("Hello world");
+    while (1) 
+    {
+    }
+}
+
